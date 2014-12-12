a48ee2393ae5559b8fe32db3f37232b5dc707c6e
Matt Keef <mkeef1@gmail.com>
2014-12-12 17:27:54 -0600

    menu state added

--- client/main.js
+++ client/main.js
@@ -1,57 +1,74 @@
 'use strict';
 
-var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'capstone', {preload: preload, create: create, update:update});
+var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'capstone'),
+  player, cursors, map, background, objects, newGameBtn, gameState,
 
-function preload(){
-  game.load.spritesheet('quasrun', '/assets/quasrun.png', 128, 128);
-  game.load.tilemap('map', '/assets/capstone.json', null, Phaser.Tilemap.TILED_JSON);
-  game.load.image('set1', '/assets/hyptosis_tile-art-batch-1.png');
-  game.load.image('set2', '/assets/hyptosis_tile-art-batch-3.png');
-}
+  menuState = {
+  preload: function(){
+    game.load.image('menu2', '/assets/menu2.jpg');
+  },
+  create: function(){
+    game.add.sprite(0, 0, 'menu2');
+    newGameBtn = game.add.text(game.world.centerX, game.world.centerY, 'New game');
+    newGameBtn.inputEnabled = true;
+  // Add input handlers
+    newGameBtn.events.onInputDown.add(startNewgame, this);
+  }
+};
 
-var player, cursors, map, background, objects;
+  function startNewgame(){
+    game.state.start('game');
+  }
 
-function create(){
-  game.physics.startSystem(Phaser.Physics.ARCADE);
-  map = game.add.tilemap('map');
-  //layer = map.createLayer('Background');
-  //layer = map.createLayer('Objects');
-  map.addTilesetImage('hyptosis_tile-art-batch-3', 'set2');
-  map.addTilesetImage('hyptosis_tile-art-batch-1', 'set1');
+  gameState = {
+  preload: function(){
+    game.load.spritesheet('quasrun', '/assets/quasrun.png', 128, 128);
+    game.load.tilemap('map', '/assets/capstone.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('set1', '/assets/hyptosis_tile-art-batch-1.png');
+    game.load.image('set2', '/assets/hyptosis_tile-art-batch-3.png');
+  },
+  create: function(){
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    map = game.add.tilemap('map');
+    //layer = map.createLayer('Background');
+    //layer = map.createLayer('Objects');
+    map.addTilesetImage('hyptosis_tile-art-batch-3', 'set2');
+    map.addTilesetImage('hyptosis_tile-art-batch-1', 'set1');
 
-  //objects.resizeWorld();
-  background = map.createLayer('Background');
-  objects = map.createLayer('objects');
-  background.resizeWorld();
-  player = game.add.sprite(100, 600, 'quasrun');
-  game.physics.arcade.enable(player);
-  cursors = game.input.keyboard.createCursorKeys();
-  game.camera.follow(player);
-  player.body.collideWorldBounds = true;
-  map.setCollision(1724, true, 'objects');
-  player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, true);
-}
-
-function update(){
-  game.physics.arcade.collide(player, objects);
-  player.body.velocity.x = 0;
-  player.animations.play('left');
-  if(cursors.left.isDown){
-    player.body.velocity.x = -150;
+    //objects.resizeWorld();
+    background = map.createLayer('Background');
+    objects = map.createLayer('objects');
+    background.resizeWorld();
+    player = game.add.sprite(100, 600, 'quasrun');
+    game.physics.arcade.enable(player);
+    cursors = game.input.keyboard.createCursorKeys();
+    game.camera.follow(player);
+    player.body.collideWorldBounds = true;
+    map.setCollision(1724, true, 'objects');
+    player.animations.add('left', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, true);
+  },
+  update: function(){
+    game.physics.arcade.collide(player, objects);
+    player.body.velocity.x = 0;
     player.animations.play('left');
+    if(cursors.left.isDown){
+      player.body.velocity.x = -150;
+      player.animations.play('left');
+    }
+    else if(cursors.right.isDown){
+      player.body.velocity.x = 150;
+      player.animations.play('left');
+    }else{
+      player.animations.stop();
+      player.frame = 11;
+    }
+    if(cursors.up.isDown && player.body.onFloor()){
+      player.body.velocity.y = -350;
+    }
+    player.body.gravity.y = 1000;
   }
-  else if(cursors.right.isDown){
-    player.body.velocity.x = 150;
-    player.animations.play('left');
-  }else{
-    player.animations.stop();
-    player.frame = 11;
-  }
-  if(cursors.up.isDown && player.body.onFloor()){
-    player.body.velocity.y = -350;
-  }
-  player.body.gravity.y = 1000;
-}
+};
 
-game.state.add('menu');
-game.state.add('load');
+game.state.add('menu', menuState);
+game.state.add('game', gameState);
+game.state.start('menu');
